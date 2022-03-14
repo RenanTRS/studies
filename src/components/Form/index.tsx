26c355e58d409e527d67bb9c31f7f94fd89c0da2
Renan Souza <renantrsouza@gmail.com>
2022-03-14 14:09:48 -0400

    add style for form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,13 +1,14 @@
+import './style.scss'
 import { Button } from "../Button"
 
 export const Form = () => {
     return(
-        <form>
-            <div>
+        <form className="novaTarefa">
+            <div className='inputContainer'>
                 <label htmlFor="task">Task</label>
                 <input type="text" id="task" placeholder="O que você precisa estudar" required />
             </div>
-            <div>
+            <div className='inputContainer'>
                 <label htmlFor="time">Time</label>
                 <input type="time" id="time" placeholder="" step="1" min="00:00:00" max="01:30:00" required />
             </div>
