d4ffdfca4f363641a47e6c9fda7a8fc19e971979
Renan Souza <renantrsouza@gmail.com>
2022-03-15 18:13:59 -0400

    Use select state, and create time state, add useEffect to change select

--- src/components/Cron/index.tsx
+++ src/components/Cron/index.tsx
@@ -1,12 +1,24 @@
 import style from './Cron.module.scss'
 
+import { useContext, useEffect, useState } from 'react'
+import { TaskContext } from '../../App'
 import { Watch } from "./Watch"
 import { Button } from "../Button"
+import { timeToSeconds } from '../../common/utils/time'
 
 export const Cron = () => {
+    const {select} = useContext(TaskContext)
+    const [time, setTime] = useState<number>()
+
+    useEffect(()=>{
+        if(select?.time){
+            setTime(timeToSeconds(select.time))
+        }
+    },[select])
+
     return(
         <div className={style.cronometro}>
-            <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+            <p className={style.titulo}>Escolha um card e inicie o cronômetro, Tempo: {time}</p>
             <div className={style.relogioWrapper}>
                 <Watch />
             </div>
